fd00aa67afd9951209315fdc0717fe628a02c0e6
Daro201 <39484304+Daro201@users.noreply.github.com>
2018-06-27 19:06:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-  function randomString() {
-    	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
-    	var str = '';
-    	for (var i = 0; i < 10; i++) {
-        	str += chars[Math.floor(Math.random() * chars.length)];
-    	}
-    	return str;
-	}
+	function randomString() {
+    		var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    		var str = '';
+    		for (var i = 0; i < 10; i++) {
+        		str += chars[Math.floor(Math.random() * chars.length)];
+    		}
+    		return str;
+	};
   
   	
 	function generateTemplate(name, data, basicElement) {
@@ -19,31 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
   		return element;
 	}
   
-function Column(name) {
-  var self = this;
+	function Column(name) {
+  		var self = this;
+		
+  		this.id = randomString();
+  		this.name = name;
+  		this.element = generateTemplate('column-template', { name: this.name, id: this.id });
+  		this.element.querySelector('.column').addEventListener('click', function (event) {
+    			if (event.target.classList.contains('btn-delete')) {
+      				self.removeColumn();
+    			}
 
-  this.id = randomString();
-  this.name = name;
-  this.element = generateTemplate('column-template', { name: this.name, id: this.id });
-
-  this.element.querySelector('.column').addEventListener('click', function (event) {
-    if (event.target.classList.contains('btn-delete')) {
-      self.removeColumn();
-    }
-
-    if (event.target.classList.contains('add-card')) {
-      self.addCard(new Card(prompt("Enter the name of the card")));
-    }
-  });
-}
+    			if (event.target.classList.contains('add-card')) {
+      				self.addCard(new Card(prompt("Enter the name of the card")));
+    			}
+  		})
+	};
   
   	Column.prototype = {
-    	addCard: function(card) {
-      		this.element.querySelector('ul').appendChild(card.element);
-    	},
-    	removeColumn: function() {
-      		this.element.parentNode.removeChild(this.element);
-    	}
+    		addCard: function(card) {
+      			this.element.querySelector('ul').appendChild(card.element);
+    		},
+    		removeColumn: function() {
+      			this.element.parentNode.removeChild(this.element);
+    		}
 	};
   
   	function Card(description) {
@@ -52,49 +51,49 @@ function Column(name) {
   		this.id = randomString();
   		this.description = description;
   		this.element = generateTemplate('card-template', { 					
-        	description: this.description 
-        }, 'li');
+        		description: this.description 
+        	}, 'li');
 		this.element.querySelector('.card').addEventListener('click', function (event) {
-    		event.stopPropagation();
-    		if (event.target.classList.contains('btn-delete')) {
-      			self.removeCard();
-    		}
+    			event.stopPropagation();
+    			if (event.target.classList.contains('btn-delete')) {
+      				self.removeCard();
+    			}
   		});
-	}
+	};
   
   	Card.prototype = {
 		removeCard: function() {
 			this.element.parentNode.removeChild(this.element);
-    	}
-	}
+    		}
+	};
  
   	var board = {
-    	name: 'Kanban Board',
-    	addColumn: function(column) {
-      		this.element.appendChild(column.element);
-      		initSortable(column.id); 
-    	},
-    	element: document.querySelector('#board .column-container')
+    		name: 'Kanban Board',
+    		addColumn: function(column) {
+      			this.element.appendChild(column.element);
+      			initSortable(column.id); 
+    		},
+    		element: document.querySelector('#board .column-container')
 	};
   
 	function initSortable(id) {
   		var el = document.getElementById(id);
   		var sortable = Sortable.create(el, {
-    		group: 'kanban',
-    		sort: true
+    			group: 'kanban',
+    			sort: true
   		});
-	}  
+	};  
   
     document.querySelector('#board .create-column').addEventListener('click', function() {
     	
     	var name = prompt('Enter a column name');
     	if (!name) {
-
     	} else {
     	var column = new Column(name);
     	board.addColumn(column);
-    }
-	});
+    	}
+
+    });
   
   // CREATING COLUMNS
 	var todoColumn = new Column('To do');
